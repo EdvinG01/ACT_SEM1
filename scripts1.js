d6b0ea9fca7078cb017dd7780da712f2713cca63
EdvinG01 <edvinguevara57@gmail.com>
2024-01-21 19:34:13 -0600

    first commit

---
+++ scripts1.js
@@ -0,0 +1,21 @@
+function mostrarMensaje1() {
+        alert("Estas son algunas sugerencias...");
+    
+    // Abre un enlace a la imagen
+    window.open("https://i.blogs.es/a9fb65/recetas-10-minutos/1366_2000.jpg") ;
+    
+  }
+
+  function mostrarMensaje2() {
+    alert("Estas son algunas sugerencias...");
+    // Abre un enlace a la imagen
+    window.open("https://i.blogs.es/5d3a74/juegos-android/450_1000.webp") ;
+    
+  }
+
+  function mostrarMensaje3() {
+    alert("Estas son algunas sugerencias...");
+    // Abre un enlace a la imagen
+    window.open("https://www.elindependiente.com/wp-content/uploads/2023/07/libros-infantiles.jpg") ;
+    
+  }
